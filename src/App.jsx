62170e3637206ad9ada0beca3839d3b8234f912f
Your Name <Sanchit Pandey>
2024-03-13 01:36:37 +0545

    players component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Players from "./components/Players";
 
 function App() {
   
 
   return (
-   <p>Cominng Soon...</p>
-  )
+  <main>
+    <div id="game-container">
+      <ol id="players">
+        <Players name="Sanchit" symbol="X"/>
+        <Players name="Sanchit2" symbol="O"/>
+      </ol>
+      GAME Board
+    </div> 
+  </main>
+  );
 }
 
 export default App
